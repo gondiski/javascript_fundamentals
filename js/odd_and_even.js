ffa45519b208d9576c887c6d360fae9cae11bb1f
Enoch Otieno <enochotieno@rocketmail.com>
2023-03-09 18:13:20 +0300

    Add the changes for loop to stop continous loop

---
+++ js/odd_and_even.js
@@ -0,0 +1,23 @@
+// We are creating an app to help us identify odd and even numbers between numbers 0 - 10
+
+numvalue = 0
+while (numvalue < 50){
+	numvalue++
+	k = numvalue
+	// conditional statement 
+	if (k%2 == 0){
+		console.log(k+" : is even")
+	}else{
+		console.log(k+" : is odd")
+	}
+	// while (k < 50){
+	// 	if (k%2 == 0 ){
+	// 		console.log(k+ " :is even")
+	// 	}else{
+	// 		console.log(k+ " :is odd")
+	// 	}
+	// }
+	
+	// numvalue++
+	// console.log(numvalue)
+}
